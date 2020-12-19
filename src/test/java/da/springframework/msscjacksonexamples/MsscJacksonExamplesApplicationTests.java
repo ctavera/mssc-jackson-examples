bb8702d4980cd375d0e45e481f9931ad54526fd1
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-19 15:07:01 -0500

    Initial project add.

---
+++ src/test/java/da/springframework/msscjacksonexamples/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package da.springframework.msscjacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
