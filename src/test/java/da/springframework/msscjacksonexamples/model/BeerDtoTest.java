af758d4448617ab0e70514e3165b8b4a31affd28
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-19 20:48:22 -0500

    112. Custom Deserializer with Jackson.

--- src/test/java/da/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/da/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -19,7 +19,7 @@ class BeerDtoTest extends BaseTest {
     @Test
     void testDeserialize() throws JsonProcessingException {
 //        String json = "{\"id\":\"c0a72eef-ed94-4151-a21c-9e8964593faa\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-12-19T15:01:10.889509-05:00\",\"lastUpdatedDate\":\"2020-12-19T15:01:10.889509-05:00\"}\n";
-        String json = "{\"beerId\":\"c0a72eef-ed94-4151-a21c-9e8964593faa\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2020-12-19T15:01:10.889509-05:00\",\"lastUpdatedDate\":\"2020-12-19T15:01:10.889509-05:00\"}\n";
+        String json = "{\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":\"12.99\",\"createdDate\":\"2020-12-19T20:45:21-0500\",\"lastUpdatedDate\":\"2020-12-19T20:45:21.2079355-05:00\",\"myLocalDate\":\"20201219\",\"beerId\":\"05acbf4e-e2e2-4dee-a9ab-b32e59ee252c\"}\n";
 
         BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
 
