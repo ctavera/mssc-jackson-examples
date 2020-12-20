d7fa05a51c44c221e9a2c69d3ead1dafada839f8
christian.tavera <christian.tavera@grupoargos.com.mx>
2020-12-19 19:22:49 -0500

    106. Jackson Property Naming Strategies.

--- src/test/java/da/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/da/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -1,16 +1,11 @@
 package da.springframework.msscjacksonexamples.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 @JsonTest
 class BeerDtoTest extends BaseTest {
-
-    @Autowired
-    ObjectMapper objectMapper;
 
     @Test
     void testSerializeDto() throws JsonProcessingException {
